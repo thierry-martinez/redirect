b0fac5c4f55df73fddbfbd04dcc7ed5bc9b1c4b2
Thierry Martinez <Thierry.Martinez@inria.fr>
2019-10-16 18:36:12 +0200

    Create (orphan) branch if it does not exist yet

--- snapshot.sh
+++ snapshot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-git checkout snapshot
+git checkout --orphan snapshot || git checkout snapshot
 git reset --hard origin/master
-git reset --soft origin/snapshot
+git reset --soft origin/snapshot || true
 project_name=$(grep "^(name [^)]*)$" dune-project)
 project_name=${project_name#(name }
 project_name=${project_name%)}
@@ -13,5 +13,5 @@ if git commit --message="Snapshot for commit $(git rev-parse origin/master)"
 then
   git remote set-url origin $(git remote get-url origin \
     | sed 's#^https://\([^/@]*@\)\?\([^/@]*\)/#git@\2:#')
-  git push
+  git push origin
 fi
